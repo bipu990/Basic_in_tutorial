340a70f967b36d536e8bab800473d1147168bd93
BIPU <bipu0000000@gmail.com>
2026-09-02 23:43:42 +0600

    Print table of 19 using (loop)

---
+++ Q16_Table_Of_19.c
@@ -0,0 +1,13 @@
+//Ques: Print the table of 19.
+
+#include <stdio.h>
+int main ()
+{
+    for (int i = 1;i<=10;i++)
+    {
+        int m = 19*i;
+        printf ("%d\n",m);
+    }
+
+    return 0;
+}
