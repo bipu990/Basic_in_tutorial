a8042fdc3cc129dd50da4344bf23897537b2971f
BIPU <bipu0000000@gmail.com>
2026-09-02 23:34:03 +0600

    Print Hello World n times (loop)
    Add a comment for the task of printing 'Hello World' multiple times based on user input.

---
+++ Q13_Hello_World_N_Times.c
@@ -0,0 +1,13 @@
+//Ques: Print hello world 'n' times. Take 'n' as input from user
+
+#include <stdio.h>
+int main ()
+{
+    int x;
+    scanf ("%d",&x);
+
+    for (int i = 1;i<=x;i++)
+    printf ("hellow woeld\n");
+
+    return 0;
+}
