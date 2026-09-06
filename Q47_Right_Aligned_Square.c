7e4e38cfa680ac8b7a1ca3ed07935d6974fffeb3
BIPU <bipu0000000@gmail.com>
2026-09-06 10:45:25 +0600

    Print right-aligned square pattern using (triple nested loop)

---
+++ Q47_Right_Aligned_Square.c
@@ -0,0 +1,29 @@
+/*
+   ****
+  ****
+ ****
+****
+*/
+
+#include <stdio.h>
+int main ()
+{
+    int x;
+    printf ("Input a number :");
+    scanf ("%d",&x);
+
+    for (int i=1;i<=x;i++)
+    {
+        for (int j=1;j<=x-i;j++)
+        {
+            printf (" ");
+        }
+        for (int k=1;k<=x;k++)
+        {
+            printf ("*");
+        }
+        printf ("\n");
+    }
+
+    return 0;
+}
