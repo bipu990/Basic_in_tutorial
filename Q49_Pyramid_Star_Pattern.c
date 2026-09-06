9036d8dbd78a6dae9681233d74ad8f2ad04824a7
BIPU <bipu0000000@gmail.com>
2026-09-06 10:55:40 +0600

    Print pyramid pattern using (triple nested loop)

---
+++ Q49_Pyramid_Star_Pattern.c
@@ -0,0 +1,29 @@
+/*
+   *
+  ***
+ *****
+*******
+*/
+
+#include <stdio.h>
+int main ()
+{
+    int x;
+    printf ("Input a number :");
+    scanf ("%d",&x);
+
+    for (int i=1;i<=x;i++)
+    {
+        for (int j=1;j<=x-i;j++)
+        {
+            printf (" ");
+        }
+        for (int k=1;k<=2*i-1;k++)
+        {
+            printf ("*");
+        }
+        printf ("\n");
+    }
+
+    return 0;
+}
