abe973287c90b062d3b92ef5e63af33f56230794
BIPU <bipu0000000@gmail.com>
2026-09-15 17:32:41 +0600

    (function, loop ,pointer)
    Implement a function to print the multiplication table for a user-input integer.

---
+++ Q65_Table_Of_N_Function_Pointer.c
@@ -0,0 +1,26 @@
+//Ques: Print the table of 'n'. Here 'n' is a integer which user will input.
+
+
+#include <stdio.h> 
+ 
+void table(int *a) 
+{ 
+    int m = 1; 
+    printf ("The table of %d is :\n",*a); //a
+    for (int i=1;i<=10;i++) 
+    { 
+        m = i*(*a); //i**a
+        printf ("%d\n",m); 
+    } 
+} 
+ 
+int main () 
+{ 
+    int x; 
+    printf ("Input a number :"); 
+    scanf ("%d",&x); 
+     
+    table(&x); 
+ 
+    return 0; 
+}
